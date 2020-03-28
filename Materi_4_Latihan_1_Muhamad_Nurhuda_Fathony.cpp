956e96fb634e81d567bab619aaf48414a14fe7d4
matiusanthony <62319080+matiusanthony@users.noreply.github.com>
2020-03-28 22:07:17 +0700

    Add files via upload

---
+++ Materi_4_Latihan_1_Muhamad_Nurhuda_Fathony.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int i=1;
+    for(i=1;i<=5;i++)
+    {
+
+        cout<<"Perulangan dengan for nilai i = "<<i<<endl;
+    }
+    i=1;
+    while (i>0,i<6)
+    {
+        cout<<"Perulangan dengan while nilai i = "<<i<<endl;
+        i=i+1;
+    }
+    i=1;
+    do {
+        cout<<"Perulangan dengan do-while nilai i = "<<i<<endl;
+        i=i+1;
+
+    }while (i>0,i<6);
+
+    return 0;
+}
---
+++ Materi_4_Latihan_2_Muhamad_Nurhuda_Fathony.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+
+    int i=1;
+    i=1;
+    while (i>0,i<=10)
+    {
+        cout<<"Perulangan dengan while. Nomor Antrian = "<<i<<endl;
+        i=i+1;
+    }
+
+    return 0;
+}
